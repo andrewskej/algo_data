049123f8a56b115e1e6e52020f8ef02b9cab05de
andrewskej <andrewskej@gmail.com>
2019-06-05 15:40:45 +0900

    DFS

--- graph.js
+++ graph.js
@@ -28,21 +28,82 @@ class Graph{
         delete this.adjacencyList[vertexName]
     }
 
+    //recursive
+    DFS_R(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+        
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                // console.log(visited)
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            })
+        })(start)
+        return result
+    }
+
+    //Iterative
+    DFS_I(start){
+        let S = [start]
+        let result = []
+        let visited = {}
+        visited[start] = true
+        let currentV
+        while(S.length){
+            currentV = S.pop()
+            result.push(currentV)
+
+            this.adjacencyList[currentV].forEach(neighbors => {
+                if(!visited[neighbors]){
+                    visited[neighbors] = true;
+                    S.push(neighbors)
+                }
+            })
+        }
+        return result
+    }
+
+
 }
 
-let g = new Graph()
-g.addVertex('Tokyo')
-g.addVertex('HongKong')
-g.addVertex('Shanghai')
-g.addEdge('Tokyo', 'HongKong')
-g.addEdge('HongKong', 'Shanghai')
-g.removeEdge('Tokyo', 'HongKong')
-g.removeVertex('HongKong')
-console.log(g)
+// let g = new Graph()
+// g.addVertex('Tokyo')
+// g.addVertex('HongKong')
+// g.addVertex('Shanghai')
+// g.addEdge('Tokyo', 'HongKong')
+// g.addEdge('HongKong', 'Shanghai')
+// g.removeEdge('Tokyo', 'HongKong')
+// g.removeVertex('HongKong')
+// console.log(g)
+
+let g2 = new Graph()
+g2.addVertex("A")
+g2.addVertex("B")
+g2.addVertex("C")
+g2.addVertex("D")
+g2.addVertex("E")
+g2.addVertex("F")
+g2.addEdge("A","B")
+g2.addEdge("B","D")
+g2.addEdge("D","E")
+g2.addEdge("D","F")
+g2.addEdge("F", "E")
+g2.addEdge("E", "C")
+g2.addEdge("C", "A")
+console.log(g2)
+
+console.log(g2.DFS_R("A"))
+console.log(g2.DFS_I("A"))
 
 //how to use 'delete' in Object
-let xx = {a:'aaa', b:'xyz', c:'abc'}
-let delThis = 'a'
-delete xx[delThis]
-delete xx.b
-console.log(xx)
+// let xx = {a:'aaa', b:'xyz', c:'abc'}
+// let delThis = 'a'
+// delete xx[delThis]
+// delete xx.b
+// console.log(xx)
