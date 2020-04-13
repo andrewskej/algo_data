52e5b66be4001957cde6e0ea534f732de2bb9a35
andrewskej <andrewskej@gmail.com>
2020-04-13 15:24:14 -0400

    still gotta figure out hashmap..

--- leetcode_problems/ContiguousArray.js
+++ leetcode_problems/ContiguousArray.js
@@ -13,42 +13,35 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+
+// Iterate the numbers and count the number of 0s and 1s
+// Save the first appearance of count in a hash map as hash = {count:index}
+// When the same count appears again at i, it means between hash[count] to i, the number of 0s and 1s evened out and it's back to its previous level.
+
 var findMaxLength = function(nums) {
-    if(nums.length <2) return 0;
+    let hash = {0:-1};
+    let count = 0;
+    let max = 0;
+    for (let i=0;i<nums.length;i++) {
+        if (nums[i] == 0) count--;
+        else count++;
 
-    let left, right = 0;
-    let longest = -Infinity;
-    let counter = {}
-    for(let i = 0; i < nums.length; i++){
-        counter[nums[i]] = 0
+        if (hash[count]!=null) max = Math.max(max, i - hash[count]);
+        else hash[count] = i;
+
+        console.log(i)
+        console.log(count)
+        console.log(hash)
+        console.log(max)
     }
-
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] in counter){
-            counter[nums[i]]++
-        }
-    }
-
-
-    while(right < nums.length){
-        for(let i = 1; i<nums.length; i++){
-            if(nums[i] == nums[i-1]){
-                left++;
-            }
-        }
-        
-        right++;
-
-        console.log(counter)
-    }
-
-
+    return max;
 
 };
 
 const nums1 = [0,1]
 const nums2 = [0,1,0]
-const nums3 = [0,1,0,1]
+const nums3 = [0,1,0,1,0,1,1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,0]
 //[0,1,0,0,0,1,0,1,1]
 
 // console.log(findMaxLength(nums1))
