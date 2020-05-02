0a5f5e82e9493326d3a993b2e58a1949bfdb098d
andrewskej <andrewskej@gmail.com>
2020-05-02 18:57:59 -0400

    one step applied binary search..

--- leetcode_problems/badVersion.js
+++ leetcode_problems/badVersion.js
@@ -13,17 +13,20 @@
  * @return {function}
  */
 var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
     return function(n) {
-        var left = 0
-        var right = n.length;
-
-        while(right-left !== -1){
-            
+        var left = 0;
+        var right = n;
+    
+        while (right - left !== 1) {
+            var mid = Math.floor(Number(left + right) / 2);
+    
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
         }
+        return right;
     };
 };
 
