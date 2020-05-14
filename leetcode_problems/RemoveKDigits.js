daa77ddea98831a8245e0de224da75a1195d001a
andrewskej <andrewskej@gmail.com>
2020-05-13 23:18:38 -0400

    didn't understand this

---
+++ leetcode_problems/RemoveKDigits.js
@@ -0,0 +1,49 @@
+// Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+// Note:
+// The length of num is less than 10002 and will be ≥ k.
+// The given num does not contain any leading zero.
+
+// Example 1:
+// Input: num = "1432219", k = 3
+// Output: "1219"
+// Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+// Example 2:
+// Input: num = "10200", k = 1
+// Output: "200"
+// Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+// Example 3:
+// Input: num = "10", k = 2
+// Output: "0"
+// Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    const stack = [];
+    
+    for (let i = 0; i < num.length; i++) {
+        let digit = num[i];
+        while (k > 0 && stack.length > 0 && stack[stack.length - 1] > digit) { //this one is important..
+            console.log(stack[stack.length-1],':', digit)
+            stack.pop();
+            k--;
+        }
+        stack.push(digit);
+        console.log(stack)
+
+    }
+    
+    while (k-- > 0) stack.pop(); //?
+    while (stack[0] === '0') stack.shift();
+    return stack.length > 0 ? stack.join('') : '0';
+};
+
+// console.log(removeKdigits(num="10200", k=1))
+// console.log(removeKdigits(num="10", k=2))
+console.log(removeKdigits(num="1432219", k=3))
