0fc4a934b444632dd2f7d6816bd2a47fafcd3429
andrewskej <andrewskej@gmail.com>
2020-05-14 21:54:34 -0400

    need practice on prototype base

--- leetcode_problems/Trie_prefix.js
+++ leetcode_problems/Trie_prefix.js
@@ -4,7 +4,16 @@ function Trie() {
 
 	function insert(word) {
 		let curr = root;
-		word.split('').forEach(ch => curr = curr[ch] = curr[ch] || {}); //wtf...?
+		word.split('').forEach(ch => 
+			{
+				if(curr[ch]){
+				curr = curr[ch]; // Actually curr = curr[ch] = curr[ch] 
+			} else {
+				curr[ch] = {};
+				curr = curr[ch]; 
+				// Same as curr = curr[ch] = {}
+			}
+			}); //wtf...?
 		curr.isWord = true;
 	}
 
@@ -34,3 +43,47 @@ console.log(tt)
 tt.insert('hello')
 console.log(tt.search('hell'))
 console.log(tt.startsWith('hel'))
+
+
+
+/**
+ * Initialize your data structure here.
+ */
+var Trie = function() {
+   
+};
+
+/**
+ * Inserts a word into the trie. 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+ 
+};
+
+/**
+ * Returns if the word is in the trie. 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix. 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
