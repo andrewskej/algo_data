b8fc775b5288bfa40b0e5d198e516a379eabba8f
andrewskej <andrewskej@gmail.com>
2020-06-29 20:59:24 -0400

    why can't I think of the solution...

---
+++ leetcode_problems/LongestCommonPrefix.js
@@ -0,0 +1,38 @@
+// Input: ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    //if empty given, return empty
+    if(strs.length === 0) return "";
+    //pick the first word of the list
+    const first = strs[0]
+
+    //for loop on the first word to get each char
+    for(let i = 0; i < first.length; i++){
+        //for loop on the list
+        for(let j = 0; j < strs.length; j++){
+            const other = strs[j] //rest of the words
+
+            console.log(other[i], first[i])
+
+            if(other[i] !== first[i]){
+                return other.slice(0,i)
+            }
+        }
+    }
+
+    return first;
+};
+
+// console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["car","zdwcar","car"]))
+// console.log(longestCommonPrefix(["aa", "aa"]))
