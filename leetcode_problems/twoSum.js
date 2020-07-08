42b7e09bbe1ffa7a7b3b8776d43c8c7beab3479d
andrewskej <andrewskej@gmail.com>
2020-07-07 22:44:23 -0400

    one more time: two sum

--- leetcode_problems/twoSum.js
+++ leetcode_problems/twoSum.js
@@ -3,18 +3,25 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const map = new Map();
 
-    for(let i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
-        if(complement in map){ //should be able to explain these lines
-            return [map[complement], i] //should be able to explain these lines
+ var twoSum = function(nums, target) {
+
+//practice little more to completely think out loud from the scratch for each line.............!!!!!!!!!!!!!!!!!!
+    const checker = {}
+
+    for(let num of nums){
+        let complement = target - num;  //get complement number that sums up to target with num
+
+        if(complement in checker){   
+            return [nums.indexOf(num), checker[complement]].sort() 
+        }else{
+            checker[num] = nums.indexOf(num);   //for next time we check the checker with the complement
         }
-        map[nums[i]] = i; //should be able to explain these lines
+        console.log(checker)
     }
     
 };
 
 
-console.log(twoSum([1,2,3,5,7], 9))
+
+console.log(twoSum([1,2,10,3,5,7], 9))
