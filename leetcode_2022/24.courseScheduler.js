c6abf594a489bfc5519c7a44073ed515d9f2f2f1
Yousik <yjeon@estateably.com>
2022-10-03 15:27:27 -0400

    topological sort..?

--- leetcode_2022/24.courseScheduler.js
+++ leetcode_2022/24.courseScheduler.js
@@ -53,10 +53,41 @@ const courseScheduler_BFS = (n, graph) => {
 
 
 //topological sort
-const courseScheduler_optimal = () => {
+//use inDegree array
+const courseScheduler_optimal = (n, graph) => {
+  const inDegree = new Array(n).fill(0)
+  const adjList = inDegree.map(() => [])
 
+  for(let i = 0; i < prerequisites.length; i++){
+    const pair = prerequisites[i];
+    inDegree[pair[0]]++;
+    adjList[pair[1]].push(pair[0]);
+  }
+
+  const stack = [];
+  for(let i = 0; i < inDegree.length; i++){
+    if(inDegree[i] === 0){
+      stack.push(i);
+    }
+  }
+  
+  let count = 0;
+  while(stack.length){
+    const current = stack.pop();
+    count++;
+    const adjacent = adjList[current];
+
+    for(let i = 0; i < adjacent.length; i++){
+      const next = adjacent[i];
+      inDegree[next]--;
+      if(inDegree[next] === 0){
+        stack.push(next);
+      }
+    }
+  }
+  return count === n;
 }
 
 
 console.log(courseScheduler_BFS(6, prerequisites))
-console.log(courseScheduler_optimal())
+console.log(courseScheduler_optimal(6, prerequisites))
