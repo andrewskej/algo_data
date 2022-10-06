da78963c2b1f90d2bcddeaec0de9264c93f7829f
Andrew Yousik Jeon <andrewskej@gmail.com>
2022-10-05 23:50:25 -0400

    dijkstra

---
+++ leetcode_2022/25.networkTimeDelay.js
@@ -0,0 +1,12 @@
+//there are n network nodes labelled 1 to N.
+//given a times array, containing edges represented by arrays [ u, v, w ] 
+//where u is the source node, v is the target node, w is the time taken to travel from the
+//source node to the target node.
+//send a signal from node k, return how long it takes for all nodes to receive the signal, 
+//Return -1 if it's impossible.
+
+const n = 5
+
+[
+    [1,2,9], [1,4,2], [2,5,1], [4,2,4], [4,5,6], [3,2,3], [5,3,7], [3,1,5]
+]
