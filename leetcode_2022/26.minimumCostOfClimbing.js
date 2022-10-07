460b3d609a03c0afed58aa72f9ca70992d652005
Yousik <yjeon@estateably.com>
2022-10-07 14:55:21 -0400

    basic unoptimized solution

--- leetcode_2022/26.minimumCostOfClimbing.js
+++ leetcode_2022/26.minimumCostOfClimbing.js
@@ -4,10 +4,17 @@
 //Find the minimum cost to reach the top of the staircase. 
 //Your first step can either be the fist or second step.
 
-const minimumCost = () => {
+const cost = [20,15,30,5]
 
+const minCostClimbingStairs = (cost) => {
+  const n = cost.length;
+  return Math.min(minCost(n-1, cost), minCost(n-2, cost))
 }
 
+const minCost = (i, cost) => {
+  if (i < 0 )return 0;
+  if (i === 0 || i === 1) return cost[i];
+  return cost[i] + Math.min(minCost(i-1, cost), minCost(i-2, cost))
+}
 
-
-console.log(minimumCost())
+console.log(minCostClimbingStairs(cost))
