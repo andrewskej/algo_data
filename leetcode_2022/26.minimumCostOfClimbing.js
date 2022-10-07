3b55762f52d19746f62b9425b4f437d1d3cc3327
Yousik <yjeon@estateably.com>
2022-10-07 15:09:34 -0400

    tabulation - bottom up

--- leetcode_2022/26.minimumCostOfClimbing.js
+++ leetcode_2022/26.minimumCostOfClimbing.js
@@ -12,6 +12,21 @@ const minCostClimbingStairs = (cost) => {
   return Math.min(minCost(n-1, cost, dp), minCost(n-2, cost, dp))
 }
 
+const minCostClimbingStairs2 = (cost) => {
+  const n = cost.length;
+  const dp = [];
+  for(let i = 0; i < n; i++){
+    if(i < 2){
+      dp[i] = cost[i];
+    } else {
+      dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2])
+    }
+  }
+  return Math.min(dp[n-1], dp[n-2]);
+}
+
+console.log(minCostClimbingStairs2(cost))
+
 const minCost = (i, cost, dp) => {
   if (i < 0 )return 0;
   if (i === 0 || i === 1) return cost[i];
