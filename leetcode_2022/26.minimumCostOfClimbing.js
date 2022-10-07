b7fa0342419771e62dd485d108c30649b2ec82aa
Yousik <yjeon@estateably.com>
2022-10-07 15:00:44 -0400

    memoization to make it O(n)

--- leetcode_2022/26.minimumCostOfClimbing.js
+++ leetcode_2022/26.minimumCostOfClimbing.js
@@ -8,13 +8,17 @@ const cost = [20,15,30,5]
 
 const minCostClimbingStairs = (cost) => {
   const n = cost.length;
-  return Math.min(minCost(n-1, cost), minCost(n-2, cost))
+  const dp = [];
+  return Math.min(minCost(n-1, cost, dp), minCost(n-2, cost, dp))
 }
 
-const minCost = (i, cost) => {
+const minCost = (i, cost, dp) => {
   if (i < 0 )return 0;
   if (i === 0 || i === 1) return cost[i];
-  return cost[i] + Math.min(minCost(i-1, cost), minCost(i-2, cost))
+  if (dp[i] != undefined) return dp[i];
+  
+  dp[i] = cost[i] + Math.min(minCost(i-1, cost, dp), minCost(i-2, cost, dp))
+  return dp[i]
 }
 
 console.log(minCostClimbingStairs(cost))
