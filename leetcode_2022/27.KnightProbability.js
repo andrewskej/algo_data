4a8e948b94128b812734c5a0df8a512c4411f047
Andrew Yousik Jeon <andrewskej@gmail.com>
2022-10-09 22:28:45 -0400

    fuck

--- leetcode_2022/27.KnightProbability.js
+++ leetcode_2022/27.KnightProbability.js
@@ -49,3 +49,70 @@ console.log(knightProb_recursive(n,k,r,c))
 
 
 
+const knightProb_memo = (n,k,r,c) => {
+    const dp = new Array(k+1).fill(0).map(
+        () => new Array(n).fill(0).map(() => new Array(n).fill(undefined))
+    )
+    return recurse(n, k, r, c, dp)
+}
+
+const recurse = (n,k,r,c,dp) => {
+    if(r < 0 || c < 0 || r >= n || c >= n){
+        return 0;
+    }
+    if(k === 0){
+        return 1;
+    }
+    if(dp[k][r][c] !== undefined) return dp[k][r][c];
+
+    let res = 0;
+    for(let i = 0; i < directions.length; i++){
+        const dir = directions[i];
+        res += recurse(n, k-1, r + dir[0], c + dir[1], dp ) / 8;
+    }
+    dp[k][r][c] = res
+    return dp[k][r][c]
+}
+
+//time: O(n^2 x k)
+//space: O(n^2 x k)
+console.log(knightProb_memo(n,k,r,c))
+
+
+//tabulation?
+//time: O(n^2 x k)
+//space: O(n^2)
+const knightProb_bottomUp = (n,k,r,c) => {
+    let prevDp = new Array(n).fill(0).map(() => new Array(n).fill(0))
+    let nextDp = new Array(n).fill(0).map(() => new Array(n).fill(0))
+
+    prevDp[r][c] = 1;
+
+    for(let step = 1; step <= k; step++){
+        for(let row = 0; row < n; row++){
+            for(let col = 0; col < n; col++){
+                for(let i = 0; i < directions.length; i++){
+                    const dir = directions[i];
+                    const prevRow = row + dir[0];
+                    const prevCol = col + dir[1];
+                    
+                    if(prevRow >= 0 && prevRow < n && prevCol >= 0 && prevCol < n){
+                        nextDp[row][col] += prevDp[prevRow][prevCol] / 8;
+                    }
+                }
+            }
+        }
+        prevDp = nextDp;
+        nextDp = new Array(n).fill(0).map(() => new Array(n).fill(0))
+    }
+
+    let res = 0;
+    for(let i =0; i<n; i++){
+        for(let j = 0; j < n; j++){
+            res += prevDp[i][j]
+        }
+    }
+    return res
+}
+
+console.log(knightProb_bottomUp(n,k,r,c))
